554368042b5b7cd9cb0a1e32989df60fdd8807f1
vijayashree <avijayashree06@gami.com>
2025-03-26 09:57:45 +0530

    done

---
+++ client-testimonials/src/App.jsx
@@ -0,0 +1,13 @@
+import TestimonialCard from "./components/TestimonialCard";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="Container">
+      <Card Name:"Emily Waston" Review:"It was an amazing exprience!!" />
+      <Card Name:"Vijayashree" Review:"It was good and godous to the team " />
+    </div>
+  );
+};
+
+export default App;
---
+++ client-testimonials/src/components/TestimonialCard.jsx
@@ -0,0 +1,10 @@
+const TestimonialCard=({Name,Review}) {
+  return(
+    <div className="Card">
+      <h2>{Name}</h2>
+      <p> {Review}</p>
+    </div>
+  );
+};
+
+export default TestimonilCard;
